3625a13405df6e261faa1efb4abeb6ec956b29a4
Matjaz Lipus <matjazl@gmail.com>
2016-02-07 12:59:39 +0100

    updating to standard style v6

--- test/index.js
+++ test/index.js
@@ -7,18 +7,18 @@ test.beforeEach(() =>
   stdin.removeAllListeners()
 )
 
-test.serial('get stdin', t => {
+test.serial('get stdin', (t) => {
   t.plan(1)
   setImmediate(() => {
     stdin.push('unicorns')
     stdin.emit('end')
   })
-  return getStdin().then(r => t.is(r, 'unicorns'))
+  return getStdin().then((r) => t.is(r, 'unicorns'))
 })
 
-test.serial('get stdin as a buffer', t => {
+test.serial('get stdin as a buffer', (t) => {
   t.plan(1)
-  const promise = getStdin({buffer: true}).then(r => {
+  const promise = getStdin({buffer: true}).then((r) => {
     t.is(r.toString().trim(), 'unicorns-foobar')
   })
   stdin.push('unicorns')
@@ -27,16 +27,16 @@ test.serial('get stdin as a buffer', t => {
   return promise
 })
 
-test.serial('return timeout error', t => {
+test.serial('return timeout error', (t) => {
   t.plan(1)
-  return getStdin({timeout: 10}).catch(err => {
+  return getStdin({timeout: 10}).catch((err) => {
     t.is(err.message, 'stdin timeout')
   })
 })
 
-test.serial('clear timeout on data', t => {
+test.serial('clear timeout on data', (t) => {
   t.plan(1)
-  const promise = getStdin({timeout: 10}).then(r => {
+  const promise = getStdin({timeout: 10}).then((r) => {
     t.is(r, 'unicorns')
   })
   stdin.push('unicorns')
@@ -44,10 +44,10 @@ test.serial('clear timeout on data', t => {
   return promise
 })
 
-test.serial('reject if TTY', t => {
+test.serial('reject if TTY', (t) => {
   t.plan(1)
   stdin.isTTY = true
-  return getStdin({rejectTTY: true}).catch(err => {
+  return getStdin({rejectTTY: true}).catch((err) => {
     t.is(err.message, 'stdin is TTY')
   })
 })
